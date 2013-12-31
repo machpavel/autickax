342d7902432317aa0ef0e1d8bac9b91201c3a889
machpavel <machpavel@gmail.com>
2013-12-31 18:30:43 +0100

    Desktop version of the game. Closes issue #1

---
+++ Autickax-desktop/src/cz/mff/cuni/autickax/Main.java
@@ -0,0 +1,19 @@
+package cz.mff.cuni.autickax;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+	// Runtime packing - expects assetSrc folder in a parent folder of this project
+	//	TexturePacker2.process("../assetSrc/", "../SampleGDXgame-android/assets/img/packed", "graphics");
+		
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "AutickaX";
+		cfg.useGL20 = false;
+		cfg.width = 800;
+		cfg.height = 480;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
