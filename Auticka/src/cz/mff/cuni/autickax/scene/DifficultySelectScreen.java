724b45ccd8c500c2ce977a2c769d76d43f038690
machpavel <machpavel@gmail.com>
2014-01-15 23:56:48 +0100

    Second menu added.

--- Auticka/src/cz/mff/cuni/autickax/scene/DifficultySelectScreen.java
+++ Auticka/src/cz/mff/cuni/autickax/scene/DifficultySelectScreen.java
@@ -10,13 +10,11 @@ import cz.mff.cuni.autickax.menu.MenuTextButton;
 
 public class DifficultySelectScreen extends BaseScreen {
 
-	private TextButton buttonKiddie;
 	private TextButton buttonBeginner;
 	private TextButton buttonNormal;
 	private TextButton buttonHard;
-	private TextButton buttonExtreme;	
 	
-	private final float buttonsXPositionStart = 30;
+	private final float buttonsXPositionStart = 15;
 	
 	public DifficultySelectScreen() {
 		super();
@@ -26,30 +24,10 @@ public class DifficultySelectScreen extends BaseScreen {
 		background.setSize(stageWidth, stageHeight);
 		stage.addActor(background); // stage initialized in superclass
 									// constructor
-
-		// Kiddie Button-----------------------------------------------------
-		this.buttonKiddie = new MenuTextButton (
-			"dìcko",
-			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY),
-			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY_HOVER)
-		)
-		{
-			@Override
-			public void action() {
-				if (Autickax.levelSelectScreen != null) {
-					Autickax.levelSelectScreen.dispose();
-					Autickax.levelSelectScreen = null;
-				}
-				Autickax.levelSelectScreen = new LevelSelectScreen(Difficulty.Kiddie);
-				getGame().setScreen(Autickax.levelSelectScreen);
-			}
-		};
-		this.buttonKiddie.setPosition(this.buttonsXPositionStart, 340);
-		stage.addActor(this.buttonKiddie);
 		
 		// Beginner Button-----------------------------------------------------
 		this.buttonBeginner = new MenuTextButton (
-			"lehký",
+			"easy",
 			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY),
 			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY_HOVER)
 		)
@@ -64,12 +42,12 @@ public class DifficultySelectScreen extends BaseScreen {
 				getGame().setScreen(Autickax.levelSelectScreen);
 			}
 		};
-		this.buttonBeginner.setPosition(this.buttonsXPositionStart, 260);
+		this.buttonBeginner.setPosition(this.buttonsXPositionStart, 350);
 		stage.addActor(this.buttonBeginner);
 		
 		// Normal Button-----------------------------------------------------
 		this.buttonNormal = new MenuTextButton (
-			"normální",
+			"medium",
 			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY),
 			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY_HOVER)
 		)
@@ -84,12 +62,12 @@ public class DifficultySelectScreen extends BaseScreen {
 				getGame().setScreen(Autickax.levelSelectScreen);
 			}
 		};
-		this.buttonNormal.setPosition(this.buttonsXPositionStart, 180);
+		this.buttonNormal.setPosition(this.buttonsXPositionStart, 250);
 		stage.addActor(this.buttonNormal);
 		
 		// Hard Button-----------------------------------------------------
 		this.buttonHard = new MenuTextButton (
-			"tìžký",
+			"hard",
 			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY),
 			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY_HOVER)
 		)
@@ -104,28 +82,8 @@ public class DifficultySelectScreen extends BaseScreen {
 				getGame().setScreen(Autickax.levelSelectScreen);
 			}
 		};
-		this.buttonHard.setPosition(this.buttonsXPositionStart, 100);
+		this.buttonHard.setPosition(this.buttonsXPositionStart, 150);
 		stage.addActor(this.buttonHard);
-		
-		// Extreme Button-----------------------------------------------------
-		this.buttonExtreme = new MenuTextButton (
-			"extrémní",
-			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY),
-			getGame().assets.getGraphics(Constants.BUTTON_MENU_DIFFICULTY_HOVER)
-		)
-		{
-			@Override
-			public void action() {
-				if (Autickax.levelSelectScreen != null) {
-					Autickax.levelSelectScreen.dispose();
-					Autickax.levelSelectScreen = null;
-				}
-				Autickax.levelSelectScreen = new LevelSelectScreen(Difficulty.Extreme);
-				getGame().setScreen(Autickax.levelSelectScreen);
-			}
-		};
-		this.buttonExtreme.setPosition(this.buttonsXPositionStart, 20);
-		stage.addActor(this.buttonExtreme);
 	}
 	
 	@Override
