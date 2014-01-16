986f6e976a8524004a0df81381a18bbf591f0c09
miseb <miseb@atlas.cz>
2014-01-16 12:01:01 +0100

    Option to to get to the next level after completed level

--- Auticka/src/cz/mff/cuni/autickax/dialogs/CompleteLevelDialog.java
+++ Auticka/src/cz/mff/cuni/autickax/dialogs/CompleteLevelDialog.java
@@ -1,20 +1,41 @@
 package cz.mff.cuni.autickax.dialogs;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-
 import cz.mff.cuni.autickax.Autickax;
 import cz.mff.cuni.autickax.Constants;
 import cz.mff.cuni.autickax.gamelogic.GameStatistics;
-import cz.mff.cuni.autickax.gamelogic.SubLevel;
+import cz.mff.cuni.autickax.gamelogic.SubLevel2;
 import cz.mff.cuni.autickax.scene.GameScreen;
 
 public class CompleteLevelDialog extends DecisionDialog {
 	GameStatistics stats;
-	public CompleteLevelDialog(GameScreen gameScreen, SubLevel subLevel, GameStatistics stats) {
-		super(gameScreen, subLevel, "", true);
+	SubLevel2 subLevel2;
+	public CompleteLevelDialog(GameScreen gameScreen, SubLevel2 subLevel2, GameStatistics stats) {
+		super(gameScreen, subLevel2, "", true);
 		this.stats = stats;
-	}
+		this.subLevel2 = subLevel2;
+	}	
 	
-	
+	@Override
+	protected void CreateButtonContinue() {
+		buttonContinue = new DialogButton(
+				Autickax.getInstance().assets
+						.getGraphics(Constants.DECISION_DIALOG_BUTTON_CONTINUE_TEXTURE),
+				Autickax.getInstance().assets
+						.getGraphics(Constants.DECISION_DIALOG_BUTTON_CONTINUE_OVER_TEXTURE)) {
 
+			@Override
+			public void action() {
+				status = DialogAbstractStatus.FINISHED;
+				decision = DecisionType.CONTINUE;				
+				subLevel2.onLevelComplete();
+				endCommunication();
+			}
+		};
+		buttonContinue.setPosition(
+				Constants.DECISION_DIALOG_BUTTON_CONTINUE_POSITION_X
+						- buttonContinue.getWidth() / 2,
+				Constants.DECISION_DIALOG_BUTTON_CONTINUE_POSITION_Y
+						- buttonContinue.getHeight() / 2);
+		this.stage.addActor(buttonContinue);
+	}
 }
