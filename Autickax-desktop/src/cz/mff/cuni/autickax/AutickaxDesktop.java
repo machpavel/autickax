0c47c45eee2a2e4a67bc1d9b149642f65195b382
Miroslav Štola <mirastola@seznam.cz>
2014-05-11 16:37:31 +0200

    Builder fixed to support icons. closes #125

--- Autickax-desktop/src/cz/mff/cuni/autickax/AutickaxDesktop.java
+++ Autickax-desktop/src/cz/mff/cuni/autickax/AutickaxDesktop.java
@@ -1,5 +1,6 @@
 package cz.mff.cuni.autickax;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -15,9 +16,9 @@ public class AutickaxDesktop {
 		cfg.height = 460;
 		cfg.fullscreen = false;
 		cfg.resizable = false;
-		//cfg.addIcon("icons/128.png", FileType.Internal);
-		//cfg.addIcon("icons/32.png", FileType.Internal);
-		//cfg.addIcon("icons/16.png", FileType.Internal);
+		cfg.addIcon("icons/128.png", FileType.Internal);
+		cfg.addIcon("icons/32.png", FileType.Internal);
+		cfg.addIcon("icons/16.png", FileType.Internal);
 		
 		new LwjglApplication(new Autickax(), cfg);
 	}
