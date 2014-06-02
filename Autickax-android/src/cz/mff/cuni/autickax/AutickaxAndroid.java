79836b1a00397c1f741d266575b637acfa279146
miseb <miseb@github.com>
2014-06-02 14:16:55 +0200

    Sets the right android app name. Turns on screen lock. #200

--- Autickax-android/src/cz/mff/cuni/autickax/AutickaxAndroid.java
+++ Autickax-android/src/cz/mff/cuni/autickax/AutickaxAndroid.java
@@ -1,18 +1,18 @@
 package cz.mff.cuni.autickax;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AutickaxAndroid extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        
-		
-        initialize(new Autickax(), cfg);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		initialize(new Autickax(), cfg);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
 }
