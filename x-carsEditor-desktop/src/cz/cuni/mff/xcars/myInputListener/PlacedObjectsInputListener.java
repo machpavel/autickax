78a3c991a3c09aa7db6f577e54ad0a9c12e4d871
miseb <miseb@github.com>
2014-06-05 10:31:56 +0200

    Removing object in editor bug has been corrected.

--- x-carsEditor-desktop/src/cz/cuni/mff/xcars/myInputListener/PlacedObjectsInputListener.java
+++ x-carsEditor-desktop/src/cz/cuni/mff/xcars/myInputListener/PlacedObjectsInputListener.java
@@ -4,38 +4,33 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 import cz.cuni.mff.xcars.EditorScreen;
 import cz.cuni.mff.xcars.constants.Constants;
-import cz.cuni.mff.xcars.entities.GameObject;
 import cz.cuni.mff.xcars.pathway.Arrow;
 
 public class PlacedObjectsInputListener extends MyInputListener {
 	private final Object representedObject;
 
-	public PlacedObjectsInputListener(Object representedObject, EditorScreen screen) {
+	public PlacedObjectsInputListener(Object representedObject,
+			EditorScreen screen) {
 		super(screen);
 		this.representedObject = representedObject;
 	}
 
 	@Override
-	public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+	public boolean touchDown(InputEvent event, float x, float y, int pointer,
+			int button) {
 		this.screen.draggedObject = representedObject;
-		if(representedObject instanceof Arrow){
-			this.screen.lastArrowMoved = (Arrow)representedObject;
+		if (representedObject instanceof Arrow) {
+			this.screen.lastArrowMoved = (Arrow) representedObject;
 		}
-		return super.touchDown(event, x, Constants.WORLD_HEIGHT - y, pointer, button);
+		return super.touchDown(event, x, Constants.WORLD_HEIGHT - y, pointer,
+				button);
 	}
-	
+
 	@Override
 	public void touchUp(InputEvent event, float x, float y, int pointer,
 			int button) {
-		if (this.representedObject instanceof GameObject) {
-			GameObject draggedGameObject = (GameObject) this.representedObject;
-			if (!this.screen.isInWorld(draggedGameObject))
-				draggedGameObject.remove();
-
-		} else if (this.representedObject instanceof Arrow) {
-			Arrow draggedArrow = (Arrow) this.representedObject;
-			if (!this.screen.isInWorld(draggedArrow))
-				draggedArrow.remove();
+		if (!this.screen.isInWorld(this.representedObject)) {
+			this.screen.removeObject(this.representedObject);
 		}
 		super.touchUp(event, x, y, pointer, button);
 	}
