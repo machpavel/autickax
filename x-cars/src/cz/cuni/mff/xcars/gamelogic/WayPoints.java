3d2344a8af189752820e6f1686a1bf2f378ba6d3
miseb <miseb@github.com>
2014-06-16 17:31:21 +0200

    Waypoints drawing only in debug correction. Reopen #221

--- x-cars/src/cz/cuni/mff/xcars/gamelogic/WayPoints.java
+++ x-cars/src/cz/cuni/mff/xcars/gamelogic/WayPoints.java
@@ -19,11 +19,10 @@ import cz.cuni.mff.xcars.input.Input;
 import cz.cuni.mff.xcars.pathway.Pathway;
 
 /**
- *	This class represents positions on the track
- *	that need to be crossed in order to successfully
- *	finish the race.
+ * This class represents positions on the track that need to be crossed in order
+ * to successfully finish the race.
  */
-public class WayPoints extends Actor{
+public class WayPoints extends Actor {
 	/** Coordinates to check whether whole track was raced through */
 	private LinkedList<Vector2> wayPoints;
 	private float sumRadii;
@@ -31,77 +30,71 @@ public class WayPoints extends Actor{
 	private Start start;
 	private Pathway pathway;
 	private ShapeRenderer renderer;
-	
-	public WayPoints(Finish finish,Start start, Pathway pathway)
-	{
+
+	public WayPoints(Finish finish, Start start, Pathway pathway) {
 		this.renderer = new ShapeRenderer();
 		this.wayPoints = new LinkedList<Vector2>();
 		this.finish = finish;
 		this.start = start;
-		this.sumRadii = finish.getBoundingRadius() + Constants.misc.MAX_DISTANCE_FROM_PATHWAY;
+		this.sumRadii = finish.getBoundingRadius()
+				+ Constants.misc.MAX_DISTANCE_FROM_PATHWAY;
 		this.pathway = pathway;
 	}
-	
 
-	
 	public void initWayPoints(float start, float finish, int nofWayPoints) {
 		this.wayPoints.clear();
 		float step = (finish - start) / nofWayPoints;
 		Vector2 lastAdded = null;
 		for (float f = start; f < finish; f += step) {
-			//do not add waypoints too close to finish
+			// do not add waypoints too close to finish
 			Vector2 pathPosition = this.pathway.GetPosition(f);
-			float sumRadii2 = sumRadii*sumRadii;
-			if (distToFinish2(pathPosition) >   sumRadii2  && distToStart2(pathPosition) > sumRadii2)
-			{
+			float sumRadii2 = sumRadii * sumRadii;
+			if (distToFinish2(pathPosition) > sumRadii2
+					&& distToStart2(pathPosition) > sumRadii2) {
 				lastAdded = pathPosition;
 				this.wayPoints.add(lastAdded);
 			}
 		}
 	}
-	
-	private float distToFinish2(Vector2 pos)
-	{
+
+	private float distToFinish2(Vector2 pos) {
 		return new Vector2(pos).sub(this.finish.getPosition()).len2();
 	}
-	
-	private float distToStart2(Vector2 pos)
-	{
+
+	private float distToStart2(Vector2 pos) {
 		return new Vector2(pos).sub(this.start.getPosition()).len2();
 	}
-	
-	public Vector2 peekFirst()
-	{
+
+	public Vector2 peekFirst() {
 		return this.wayPoints.peekFirst();
 	}
-	
-	public Vector2 removeFirst()
-	{
+
+	public Vector2 removeFirst() {
 		return this.wayPoints.removeFirst();
 	}
-	
-	public boolean isEmpty()
-	{
+
+	public boolean isEmpty() {
 		return this.wayPoints.isEmpty();
 	}
-	
-	public void draw(Batch batch, float parentAlpha)
-	{
-		batch.end();		
-		Gdx.gl.glEnable(GL20.GL_BLEND);
-		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		if (Debug.DEBUG && Debug.drawWayPoints)
-		{
+
+	public void draw(Batch batch, float parentAlpha) {
+		if (Debug.DEBUG && Debug.drawWayPoints) {
+			batch.end();
+			// TODO why is here blend function when we dont use any alpha??? And
+			// you don't disable it again. For faster performance.
+			Gdx.gl.glEnable(GL20.GL_BLEND);
+			Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
 			this.renderer.begin(ShapeType.Line);
 			this.renderer.setColor(Color.BLACK);
-			for(Vector2 way: this.wayPoints)
-			{
-				this.renderer.circle(way.x * Input.xStretchFactorInv, way.y* Input.yStretchFactorInv,
-						Constants.misc.MAX_DISTANCE_FROM_PATHWAY*Input.xStretchFactorInv);
-				
+			for (Vector2 way : this.wayPoints) {
+				this.renderer.circle(way.x * Input.xStretchFactorInv, way.y
+						* Input.yStretchFactorInv,
+						Constants.misc.MAX_DISTANCE_FROM_PATHWAY
+								* Input.xStretchFactorInv);
 			}
 			renderer.end();
+			batch.begin();
 		}
-		batch.begin();
 	}
 }
