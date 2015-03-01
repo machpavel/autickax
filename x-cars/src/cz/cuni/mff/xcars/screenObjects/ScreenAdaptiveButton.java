8b0e5e8eb76b6e5428b6a98a6eefa6d691e84195
miseb <miseb@github.com>
2015-03-01 14:04:44 +0100

    Buttons don't have "over". Buttons react only when "up" event is called within the button range.

--- x-cars/src/cz/cuni/mff/xcars/screenObjects/ScreenAdaptiveButton.java
+++ x-cars/src/cz/cuni/mff/xcars/screenObjects/ScreenAdaptiveButton.java
@@ -4,34 +4,23 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public abstract class ScreenAdaptiveButton extends Button {
 
-	public ScreenAdaptiveButton(TextureRegion image, TextureRegion imageHover) {
-		super(new ScreenAdaptiveButtonStyle(image, imageHover));
+	public ScreenAdaptiveButton(TextureRegion image, TextureRegion imagePressed) {
+		super(new ScreenAdaptiveButtonStyle(image, imagePressed));
 
 		this.addListener(new InputListener() {
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
 				return true;
 			}
 
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				action();
-			}
-		});
-	}
-
-	public ScreenAdaptiveButton(Drawable up) {
-		super(up);
-
-		this.addListener(new InputListener() {
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				action();
+			public void touchUp(InputEvent event, float x, float y,
+					int pointer, int button) {
+				if (x >= 0 && x < getWidth() && y >= 0 && y < getHeight()) {
+					action();
+				}
 			}
 		});
 	}
